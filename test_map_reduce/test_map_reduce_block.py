c85802752ba044e76b472c36d935ae53721c824f
Matthew Dvorsky <matthew.ryan.dvorsky@gmail.com>
2026-03-05 20:44:52 -0600

    Added vmapped block version, not efficient at all.

--- test_map_reduce/test_map_reduce_block.py
+++ test_map_reduce/test_map_reduce_block.py
@@ -15,21 +15,30 @@ def _kernel(x, y):
 
 @jax.jit
 def test1(x, y):
-    return map_reduce(_kernel, x, y, axis=[2, 3], unroll_count=1)
+    return map_reduce(_kernel, x, y, axis=[1, 2], unroll_count=32)
 
 
+# @jax.jit
+# def test2(x, y):
+#     a = jnp.squeeze(x, axis=[0])
+
+#     def _test2_helper(b):
+#         print(a, b)
+#         return jnp.sum(_kernel(a, b))
+
+#     return jax.lax.map(_test2_helper, y, batch_size=16)
 
 
 key = jax.random.PRNGKey(10)
-x = jax.random.normal(key, [1, 1, 32, 32])
-y = jax.random.normal(key, [2000, 3000, 1, 1])
+x = jax.random.normal(key, [1*1, 1024, 2000])
+y = jax.random.normal(key, [32*32, 1, 1])
 
 
 export_graph(test1, x, y)
-# export_graph(test4, x, y)
+# export_graph(test2, x, y)
 
 time_it(test1, x, y)
-# time_it(test4, x, y)
+# time_it(test2, x, y)
 
-# print(jnp.max(jnp.abs(test3(x, y))))
-# print(jnp.max(jnp.abs(test4(x, y) - test3(x, y))))
+print(jnp.max(jnp.abs(test1(x, y))))
+# print(jnp.max(jnp.abs(test2(x, y) - test1(x, y))))
